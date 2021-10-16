1c4011c00651363c4bd567dd9b0e6f95ecfaafc6
KatherineYuneman <kyuneman@gmail.com>
2021-10-15 22:08:07 -0400

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,57 @@
-// Code your solution in this file!
+
+//distance from HQ in BLOCKS
+
+const start = 34;
+const destination = 55;
+console.log(`Starting Block: ${start} Street`);
+console.log(`Destination Block: ${destination} Street`);
+
+function distanceFromHqInBlocks(start){
+   if (start >= 42){
+       return (start - 42);
+    } else if (start < 42){
+        return (42 - start);
+    }
+}
+
+const returnDistanceBlocks = console.log("Distance from HQ in Blocks:", distanceFromHqInBlocks(start));
+
+// distance from HQ in FEET
+
+function distanceFromHqInFeet(){
+    let blocks = distanceFromHqInBlocks(start);
+    return (blocks * 264);
+}
+console.log("Distance from HQ in Feet:",distanceFromHqInFeet());
+
+//distance travelled in FEET
+
+function distanceTravelledInFeet(start, destination){
+    if (start > destination){
+       return ((start - destination) * 264)
+    } else if (destination > start){
+        return ((destination - start) * 264)
+}};
+console.log("Distance Traveled in Feet:",distanceTravelledInFeet(start,destination));
+
+
+// fare price calculation
+
+let distanceFeet = distanceTravelledInFeet(start, destination);
+
+function calculatesFarePrice(start,destination){
+    if (distanceFeet <= 400){
+        return 0;
+    }
+    else if (distanceFeet > 400 && distanceFeet <= 2000){
+        return ((distanceFeet - 400) * .02);
+        
+    }
+    else if (distanceFeet > 2000 && distanceFeet < 2500){
+        return 25;
+    }
+    else if (distanceFeet > 2500) {
+        return 'cannot travel that far'
+    }
+}
+console.log("Fare Price:",calculatesFarePrice(start,destination));
